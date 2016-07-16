46a0205f2d37da4286c8dfcbc47168076586dee3
Mike <keeper96@gmail.com>
2016-07-17 00:03:01 +0300

    DOM Hometask

---
+++ 05-dom/js/script.js
@@ -0,0 +1,38 @@
+//  ДЗ #1 
+function prepend(container, newElement) {	
+	let cont = document.querySelector(container);
+	let newEl = document.createElement('div');
+	newEl.classList.add('new-element');
+	newEl.innerText = newElement;
+	//let newEl = document.querySelector(newElement); С
+	cont.insertBefore(newEl, cont.firstChild);
+}
+prepend('.container', 'Гарик');
+
+
+//  ДЗ #2
+function deleteTextNodes(el) {	
+	let target = document.querySelector(el);
+	let childNodes = target.childNodes;
+	
+	// for (let i=0; i < childNodes.length; i++) {
+	// 	let node = childNodes[i];
+	// 	if(node.nodeType == 3){
+	// 		target.removeChild(node);
+	// 	}
+	// }
+
+	//ES6
+	for(let node of childNodes) { // данный тип итератора доступен в пседомассивах
+		if(node.nodeType == 3){   // node - переменная в которой хранится каждый дочерний нод childNodes
+			target.removeChild(node);
+		}
+	}
+}
+deleteTextNodes('.container');
+
+
+// module.exports = {
+// 	prepend : prepend,
+// 	deleteTextNodes: deleteTextNodes
+// }
