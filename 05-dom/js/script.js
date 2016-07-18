867e66546fdd796540a47dfa7ae8f812ce7d730f
Mike <keeper96@gmail.com>
2016-07-18 23:12:56 +0300

    Final version with comments

--- 05-dom/js/script.js
+++ 05-dom/js/script.js
@@ -1,10 +1,9 @@
 //  ДЗ #1 
 function prepend(container, newElement) {	
-	let cont = document.querySelector(container);
-	let newEl = document.createElement('div');
-	newEl.classList.add('new-element');
-	newEl.innerText = newElement;
-	//let newEl = document.querySelector(newElement); Уточнить нужно ли создавать новый элемент иои использовать уже существующий
+	let cont = document.querySelector(container); 
+	let newEl = document.createElement('div'); // Создаем новый <div>
+	newEl.classList.add('new-element'); // Добавляем ему класс
+	newEl.innerText = newElement; // Добавляем ему контент
 	cont.insertBefore(newEl, cont.firstChild);
 }
 prepend('.container', 'Гарик');
@@ -14,25 +13,18 @@ prepend('.container', 'Гарик');
 function deleteTextNodes(el) {	
 	let target = document.querySelector(el);
 	let childNodes = target.childNodes;
-	
-	// for (let i=0; i < childNodes.length; i++) {
-	// 	let node = childNodes[i];
-	// 	if(node.nodeType == 3){
-	// 		target.removeChild(node);
-	// 	}
-	// }
-
 	//ES6
 	for(let node of childNodes) { // данный тип итератора доступен в пседомассивах
 		if(node.nodeType == 3){   // node - переменная в которой хранится каждый дочерний нод childNodes
 			target.removeChild(node);
-		}
+		}		
 	}
+	console.log(childNodes);
 }
 deleteTextNodes('.container');
 
-
-// module.exports = {
-// 	prepend : prepend,
-// 	deleteTextNodes: deleteTextNodes
-// }
+// Експорт модулей
+module.exports = {
+	prepend : prepend,
+	deleteTextNodes: deleteTextNodes
+}
