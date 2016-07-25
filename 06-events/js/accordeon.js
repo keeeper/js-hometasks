d0fcb7b128c364344d4a0c83e075a90ac26fdb3f
Mike <keeper96@gmail.com>
2016-07-25 20:27:47 +0300

    Accordeon updated

--- 06-events/js/accordeon.js
+++ 06-events/js/accordeon.js
@@ -1,52 +1,23 @@
-// Кроссбраузерность
-var eventsObj = {
-  addEvent: function (el, type, fn) {
-    if (typeof addEventListener !== 'undefined') {
-      el.addEventListener(type, fn, false);
-    } else if (typeof attachEvent !== 'undefined') {
-      el.attachEvent('on' + type, fn);
-    } else {
-      el['on' + type] = fn;
-    }
-  },
-
-  getTarget: function (event) {
-    if (typeof event.target !== 'undefined') {
-      return event.target;
-    } else {
-      return event.srcElement;
-    }
-  },
-
-  preventDefault: function (event) {
-    if (typeof event.preventDefault !== 'undefined') {
-      event.preventDefault();
-    } else {
-      event.returnValue = false;
-    }
-  }
-};
-
-// Аккордеон
 (function() {
-  var toggleState = function (e) {
-    eventsObj.preventDefault(e);
+    let accordeon = document.querySelector('.accordeon');
 
-    var elem = eventsObj.getTarget(e),
-        elems = document.getElementsByClassName('accordeon__trigger');
-        dataTrigger = elem.getAttribute('data-trigger');
+    let toggleState = function (e) {
+        let element = e.target; // Элемент по которому кликнули
+            elems = document.querySelectorAll('.accordeon__trigger'); // Все элементы аккордеона
 
-    for (var i=0; i < elems.length; i++) {
-      if(elems[i].classList.contains('active')){
-        elems[i].classList.remove('active');
-        elems[i].classList.add('is-hidden');
-      }
+        // Закрытие всех ранее открытых элементов аккордеона
+        for (let el of elems) {
+            if(el.classList.contains('active')){
+                el.classList.remove('active');
+                el.classList.add('is-hidden');
+            }
+        }
+        
+        // Открытие элемента по которому кликнули
+        if (element.classList.contains('is-hidden')) {
+            element.classList.remove('is-hidden');
+            element.classList.add('active');
+        }
     }
-    if (dataTrigger) {
-      elem.classList.toggle('is-hidden');
-      elem.classList.toggle('active');
-    }
-  }
-
-  eventsObj.addEvent(document, 'click', toggleState);
+    accordeon.addEventListener('click', toggleState);
 })();
