3372c2f3c22adf7f05e0391ead46d656aa5b03a9
Mike <keeper96@gmail.com>
2016-08-16 23:14:15 +0300

    Date format is fixed

--- 09-api-sdk/02-vk-friends/script.js
+++ 09-api-sdk/02-vk-friends/script.js
@@ -21,7 +21,7 @@ new Promise(function(resolve) {
   });
 }).then(function() { // Получаем заголовок
   return new Promise(function(resolve, reject) {
-    VK.api('users.get', {'name_case': 'gen', 'user_id': '372216738'},  function(response) {
+    VK.api('users.get', {'name_case': 'gen'},  function(response) {
       if (response.error) {
         reject(new Error(response.error.error_msg));
       } else {
@@ -32,19 +32,32 @@ new Promise(function(resolve) {
   })
 }).then(function() { // Получаем и обрабатываем список друзей
   return new Promise(function(resolve, reject) {
-    VK.api('friends.get', {'user_id': '372216738', fields:'photo_50, first_name, last_name, bdate'}, function(response) {
+    VK.api('friends.get', {fields:'photo_50, first_name, last_name, bdate'}, function(response) {
       if (response.error) {
         reject(new Error(response.error.error_msg));
       } else {
         // Получение возраста из даты рождения и добавление его в response.response
         for (let i = 0; i < response.response.length; i++){
-          date = response.response[i].bdate;
+            date = response.response[i].bdate;
 
+            // Функция преобразования даты в формат гггг/мм/дд для последующей обработки
+            function toDate(dateStr) {
+                var parts = dateStr.split(".");
+                if (parts.length == 3) {
+                    return new Date(parts[2], parts[1] - 1, parts[0]); // Определение даты, если дата роджения указана полностью
+                } else if (parts.length == 2) {
+                    return new Date("", parts[1] - 1, parts[0]); // Определение даты, если дата роджения указана не полностью
+                }
+            }
 
             // Если пользователь указал дату рождения
           if (date) {
               // Вычисление возраста из bdate возвращенного VK.api
-              let age = ((new Date().getTime() - new Date(date)) / (24 * 3600 * 365.25 * 1000)) | 0;
+              let age = ((new Date().getTime() - new Date(toDate(date))) / (24 * 3600 * 365.25 * 1000)) | 0;
+              // Не выводим недостоверную информацию о возрасте
+              if (age > 110) { // Если кто-то "накинул" себе годиков — обнуляем
+                  age = null;
+              }
 
               // Добавление возраста в объект response.response для последующей передачи в handlebars
               Object.defineProperty(response.response[i], 'age', {
@@ -56,31 +69,26 @@ new Promise(function(resolve) {
               // Тяжелые манипуляции с датой рождения
               let dateObj = new Date(date);
               let curDate = new Date(),
-                  year = curDate.getFullYear(),
-                  month = dateObj.getMonth(),
-                  birthdate = dateObj.getDate(),
                   birthday = new Date();
-
               // Меняем формат даты - месяц и день рождения местами (с дд/мм на мм/дд)
-              birthday.setFullYear(year);
-              birthday.setMonth(birthdate - 1);
-              birthday.setDate(month + 1);
+              birthday = toDate(date);
+              // Преобразовываем дату рождения в день рождения в текущем году
+              birthday.setFullYear(curDate.getFullYear());
 
               // Сравнение даты рождения с сегодняшним днем
               let div = +birthday - curDate;
               if (div < 0) {
-                  birthday.setFullYear(year + 1);
+                  birthday.setFullYear(curDate.getFullYear() + 1);
                   div = +birthday - curDate;
               }
-
               Object.defineProperty(response.response[i], 'timeBefore', {
                   value: div,
                   enumerable: true,
                   writable: true
               });
-          }  else {
-              response.response[i]['timeBefore'] = 31528800000;
-              response.response[i]['age'] = null;
+          } else { // Если дата рождения не указана
+             response.response[i]['timeBefore'] = 31622500000; // то время до дня рождения больше года, т.е. эти пользователи будут выведены в конце списка
+             response.response[i]['age'] = null; // возраст - неопределен
           }
 
         }
